1a5bc81bb0693a40be84e59c4c1eb9fbb0d7f1d9
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2024-06-13 09:35:03 -0500

    auth commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -42,7 +42,6 @@ class LoginController extends Controller
 
     public function activity_view(Request $request)
     {
-        // Redireccionamiento para ORI y DIE
         $actions = $request->input('actions');
         $about_what = $request->input('about_what');
         $nacoInt = $request->input('nacoInt');
@@ -53,30 +52,16 @@ class LoginController extends Controller
 
 
         if ($about_what != ""  || $actions != "") {
-            if ($actions == "registrar" && $about_what == "convenios") {
-                if (auth()->user()->rol_id == '2') {
-                    return redirect('/activities/registro_convenios_nac');
-                } else if (auth()->user()->rol_id == '3') {
-                    return redirect('/activities/registro_convenios_int');
-                } else if (auth()->user()->rol_id == '6') {
-                    if ($nacoInt == 'internacional') {
-                        return redirect('/activities/registro_convenios_int');
-                    } else if ($nacoInt == 'nacional') {
-                        return redirect('/activities/registro_convenios_nac');
-                    }
-                }
-            } else if ($actions == "registrar" && $about_what == "instituciones") {
-                if (auth()->user()->rol_id == '2') {
-                    return redirect('/activities/registro_instituciones_nac');
-                } else if (auth()->user()->rol_id == '3') {
-                    return redirect('/activities/registro_instituciones_int');
-                } else if (auth()->user()->rol_id == '6') {
-                    if ($nacoInt == 'internacional') {
-                        return redirect('/activities/registro_instituciones_int');
-                    } else if ($nacoInt == 'nacional') {
-                        return redirect('/activities/registro_instituciones_nac');
-                    }
-                }
+            if ($actions == "registrar" && $about_what == "convenios" && $nacoInt == "nacional") {
+                return redirect('/activities/registro_convenios_nac');
+            } else if ($actions == "registrar" && $about_what == "convenios" && $nacoInt == "internacional") {
+                return redirect('/activities/registro_convenios_int');
+            } else if ($actions == "registrar" && $about_what == "instituciones" && $nacoInt == "nacional") {
+                return redirect('/activities/registro_instituciones_nac');
+            } else if ($actions == "registrar" && $about_what == "instituciones" && $nacoInt == "internacional") {
+                return redirect('/activities/registro_instituciones_int');
+
+
             } else if ($actions == "consultar" && $about_what == "instituciones" && $nacoInt == "internacional") {
                 return redirect('/activities/cons_instituciones_int');
             } else if ($actions == "consultar" && $about_what == "instituciones" && $nacoInt == "nacional") {
@@ -85,7 +70,6 @@ class LoginController extends Controller
                 return redirect()->route('convenios.show_int');
             } else if ($actions == "consultar" && $about_what == "convenios" && $nacoInt == "nacional") {
                 return redirect()->route('convenios.show_nac');
-
 
 
             } else if ($actions == "consultar" && $about_what == "movilidad" && $nacoInt == "nacional") {
@@ -97,61 +81,8 @@ class LoginController extends Controller
             } else if ($actions == "registrar" && $about_what == "movilidad" && $nacoInt == "internacional") {
                 return redirect('/activities/create_movilidad_int');
             }
-
-            /* 
-                ($actions == "registrar" && $about_what == "movilidad" && $nacoInt == "nacional" && $entSal == "entrante") {
-                return redirect('/activities/registro_movilidad_nac/entrante');
-            } else if ($actions == "registrar" && $about_what == "movilidad" && $nacoInt == "nacional" && $entSal == "saliente") {
-                return redirect('/activities/registro_movilidad_nac/saliente');
-            } else if ($actions == "registrar" && $about_what == "movilidad" && $nacoInt == "internacional" && $entSal == "saliente") {
-                return redirect('/activities/registro_movilidad_int/saliente');
-            } else if ($actions == "registrar" && $about_what == "movilidad" && $nacoInt == "internacional" && $entSal == "entrante") {
-                return redirect('/activities/registro_movilidad_int/entrante');
-                //rutas para consulta de movilidad
-            } else if ($actions == "consultar" && $about_what == "movilidad" && $nacoInt == "internacional" && $entSal == "entrante") {
-                return redirect('/activities/cons_movilidad_int/entrante');
-            } else if ($actions == "consultar" && $about_what == "movilidad" && $nacoInt == "nacional" && $entSal == "entrante") {
-                return redirect('/activities/cons_movilidad_nac/entrante');
-            } else if ($actions == "consultar" && $about_what == "movilidad" && $nacoInt == "internacional" && $entSal == "saliente") {
-                return redirect('/activities/cons_movilidad_int/saliente');
-            } else if ($actions == "consultar" && $about_what == "movilidad" && $nacoInt == "nacional" && $entSal == "saliente") {
-                return redirect('/activities/cons_movilidad_nac/saliente');
-            }*/
         }
-            
-        // Redireccionamiento para Coordinaciones u otras dependencias
-        $c_o_actions = $request->input('c_o_actions');
-        $c_o_about_what = $request->input('c_o_about_what');
-        $c_o_nacoInt = $request->input('c_o_nacoInt');
-        $c_o_entSal = $request->input('c_o_entSal');
-
-        if ($c_o_actions != "" && $c_o_about_what != "") {
-            if ($c_o_actions == "registrar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "internacional" && $c_o_entSal == "entrante") {
-                return redirect('/activities/registro_movilidad_int/entrante');
-            } else if ($c_o_actions == "registrar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "internacional" && $c_o_entSal == "saliente") {
-                return redirect('/activities/registro_movilidad_int/saliente');
-            } else if ($c_o_actions == "registrar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "nacional" && $c_o_entSal == "entrante") {
-                return redirect('/activities/registro_movilidad_nac/entrante');
-            } else if ($c_o_actions == "registrar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "nacional" && $c_o_entSal == "saliente") {
-                return redirect('/activities/registro_movilidad_nac/saliente');
-            } else if ($c_o_actions == "consultar" &&  $c_o_about_what == "convenio" && $c_o_nacoInt == "internacional") {
-                return redirect('/activities/cons_convenios_int');
-            } else if ($c_o_actions == "consultar" &&  $c_o_about_what == "convenio" && $c_o_nacoInt == "nacional") {
-                return redirect('/activities/cons_convenios_nac');
-            } else if ($c_o_actions == "consultar" &&  $c_o_about_what == "institucion" && $c_o_nacoInt == "internacional") {
-                return redirect('/activities/cons_instituciones_int');
-            } else if ($c_o_actions == "consultar" &&  $c_o_about_what == "institucion" && $c_o_nacoInt == "nacional") {
-                return redirect('/activities/cons_instituciones_nac');
-            } else if ($c_o_actions == "consultar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "internacional" && $c_o_entSal == "entrante") {
-                return redirect('/activities/cons_movilidad_int/entrante');
-            } else if ($c_o_actions == "consultar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "nacional" && $c_o_entSal == "entrante") {
-                return redirect('/activities/cons_movilidad_nac/entrante');
-            } else if ($c_o_actions == "consultar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "internacional" && $c_o_entSal == "saliente") {
-                return redirect('/activities/cons_movilidad_int/saliente');
-            } else if ($c_o_actions == "consultar" && $c_o_about_what == "movilidad" && $c_o_nacoInt == "nacional" && $c_o_entSal == "saliente") {
-                return redirect('/activities/cons_movilidad_nac/saliente');
-            }
-        }
+        
         return view('activities.select_activities');
     }
 
